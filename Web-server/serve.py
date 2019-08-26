d5d8c0f89920a4cc4034dab19f3d7cad0bc9bf22
tranvietphuoc <phuoc.finn@gmail.com>
2019-08-26 14:23:36 +0700

    example about a server using http.server

---
+++ Web-server/serve.py
@@ -0,0 +1,25 @@
+import http.server
+
+class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
+    # GET method
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type","text/html")
+        self.end_headers()
+
+        # then response a html file
+        self.wfile.write(b"<!DOCTYPE html>")
+        self.wfile.write(b"<html lang='en'>")
+        self.wfile.write(b"<head>")
+        self.wfile.write(b"<title>Hello, title</title>")
+        self.wfile.write(b"</head>")
+        self.wfile.write(b"<body>")
+        self.wfile.write(b"Hello,world")
+        self.wfile.write(b"</body>")
+        self.wfile.write(b"</html>")
+
+port=8080 # http's port
+server_address=("0.0.0.0",port)
+httpd=http.server.HTTPServer(server_address,HTTPServer_RequestHandler)
+httpd.serve_forever()
+# run that Python file and go to the address 0.0.0.0:8080 to see the result
