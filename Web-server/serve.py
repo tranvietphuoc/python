934525b46099354b0d657d9869abad7f0d87126d
Tran Viet Phuoc <phuoc.finn@gmail.com>
2019-09-26 15:25:03 +0700

    edit contents of serve.py

--- Web-server/serve.py
+++ Web-server/serve.py
@@ -6,23 +6,23 @@ class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
     # GET method
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type","text/html")
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
         # then response a html file
         self.wfile.write(b"<!DOCTYPE html>")
         self.wfile.write(b"<html lang='en'>")
         self.wfile.write(b"<head>")
-        self.wfile.write(b"<title>Hello, title</title>")
+        self.wfile.write(b"<title>Hello, my test page</title>")
         self.wfile.write(b"</head>")
         self.wfile.write(b"<body>")
-        self.wfile.write(b"Hello,world")
+        self.wfile.write(b"Hello, I'm tranvietphuoc")
         self.wfile.write(b"</body>")
         self.wfile.write(b"</html>")
 
 port = 8080  # http's port
-server_address = ("0.0.0.0", port)
+server_address = ('localhost', port)
 httpd = http.server.HTTPServer(server_address, HTTPServer_RequestHandler)
-print('serve at 0.0.0.0:8080')
+print('server run at http://localhost:8080')
 httpd.serve_forever()
 # run that Python file and go to the address 0.0.0.0:8080 to see the result
