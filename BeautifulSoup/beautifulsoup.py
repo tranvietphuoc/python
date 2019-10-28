986c326125ae86da3ba12adc0656a2e99513e142
Tran Viet Phuoc <phuoc.finn@gmail.com>
2019-10-29 02:01:04 +0700

    beautifulsoup

--- BeautifulSoup/beautifulsoup.py
+++ BeautifulSoup/beautifulsoup.py
@@ -4,4 +4,13 @@ from bs4 import BeautifulSoup
 with open('./index.html') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
-print(soup.get_text())
+
+
+soup = BeautifulSoup(html_doc, "html.parser")
+print(soup.prettify())
+
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+print(soup.title)
